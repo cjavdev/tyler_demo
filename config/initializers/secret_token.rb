9eaa1e522b1982f018e1b0616a1a0750c4418319
CJ Avilla <cjavilla@gmail.com>
2013-11-28 14:57:01 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TylerDemo::Application.config.secret_token = '80a22b453bd37d040a80d9f94b0f20e9f3afe2bf955b0d7bd7e88001d3c188083ce8846cf5d99044571eb2f8e3fbe053976cbfef14ad277508d9664999c7f8fa'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+TylerDemo::Application.routes.draw do
+  root to: "root#root"
+end
+
